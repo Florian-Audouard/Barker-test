32a495f3be8cbacd7d534127fd34b15b67f0a0ae
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-03-28 19:18:26 +1100

    h

--- server.py
+++ server.py
@@ -1,15 +1,35 @@
 import os
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 os.chdir(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
 
+msg = []
+
 
 @app.route("/")
 def index():  # pylint: disable=missing-function-docstring
     return render_template("index.html")
+
+
+@app.route("/msgFromServer", methods=["POST"])
+def send_msg():  # pylint: disable=missing-function-docstring
+    data = request.get_data()
+    print(data)
+    number = int(data)
+    if number == len(msg):
+        return jsonify("no")
+    print(msg[number : len(msg)])
+    return jsonify(msg[number : len(msg)])
+
+
+@app.route("/msgFromHtml", methods=["POST"])
+def receive_msg():  # pylint: disable=missing-function-docstring
+    result = request.get_data()
+    msg.append(result.decode("utf-8"))
+    return "ok"
 
 
 if __name__ == "__main__":
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,39 @@
-fetch("/getDatabase")
-	.then((res) => res.json())
-	.then((data) => {
-		document.querySelector("#testDatabase").innerText = data[0][1];
+const input = document.querySelector("input");
+const msg = document.querySelector("#msg");
+const listMsg = [];
+
+function addMsg(text) {
+	const span = document.createElement("span");
+	span.innerText = text;
+	msg.appendChild(span);
+}
+
+input.addEventListener("keydown", (event) => {
+	if (!event.code.toLowerCase().includes("enter")) {
+		return;
+	}
+	fetch("/msgFromHtml", {
+		method: "POST",
+		body: input.value,
 	});
+	input.value = "";
+});
+function getMessage() {
+	fetch("/msgFromServer", {
+		method: "POST",
+		body: listMsg.length,
+	})
+		.then((resp) => {
+			return resp.json();
+		})
+		.then((data) => {
+			if (data === "no") {
+				return;
+			}
+			for (let elem of data) {
+				listMsg.push(elem);
+				addMsg(elem);
+			}
+		});
+}
+setInterval(getMessage, 3000);
