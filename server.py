152fdb932a21f9f843c2c9ec80a52d0bbf14821c
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-03-28 11:46:22 +1100

    t

--- server.py
+++ server.py
@@ -1,23 +1,15 @@
 import os
 from flask import Flask, jsonify, render_template
-from database import reset_table, get_data
 
 os.chdir(os.path.dirname(__file__))
 
 
 app = Flask(__name__)
 
-reset_table()
-
 
 @app.route("/")
 def index():  # pylint: disable=missing-function-docstring
     return render_template("index.html")
-
-
-@app.route("/getDatabase", methods=["GET"])
-def get_database():  # pylint: disable=missing-function-docstring
-    return jsonify(get_data())
 
 
 if __name__ == "__main__":
