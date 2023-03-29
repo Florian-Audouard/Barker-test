5c91ffafa976d06298d40dcfb0b570a73dfc62a3
Florian-Audouard <45578522+Florian-Audouard@users.noreply.github.com>
2023-03-29 15:22:08 +1100

    test fichier

--- server.py
+++ server.py
@@ -6,8 +6,6 @@ os.chdir(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-msg = []
-
 
 @app.route("/")
 def index():  # pylint: disable=missing-function-docstring
@@ -17,8 +15,12 @@ def index():  # pylint: disable=missing-function-docstring
 @app.route("/msgFromServer", methods=["POST"])
 def send_msg():  # pylint: disable=missing-function-docstring
     data = request.get_data()
-    print(data)
     number = int(data)
+    msg = []
+    with open("./save.txt", "r", encoding="utf-8") as file:
+        for x in file:
+            msg.append(x.replace("\n", ""))
+    print(msg)
     if number == len(msg):
         return jsonify("no")
     print(msg[number : len(msg)])
@@ -28,7 +30,8 @@ def send_msg():  # pylint: disable=missing-function-docstring
 @app.route("/msgFromHtml", methods=["POST"])
 def receive_msg():  # pylint: disable=missing-function-docstring
     result = request.get_data()
-    msg.append(result.decode("utf-8"))
+    with open("./save.txt", "a", encoding="utf-8") as file:
+        file.write(result.decode("utf-8") + "\n")
     return "ok"
 
 
